2bbf7fdef87b02b3032af4396f50d8456318ac0d
Patrick Gendotti <patrick.gendotti@gmail.com>
2019-01-22 12:18:26 -0800

    feat: Update 'default' task to watch the sass folder for any changes and update the css folder based on those changes

--- my-project/gulpfile.js
+++ my-project/gulpfile.js
@@ -10,9 +10,14 @@ var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer')
 //var print = require('gulp-print').default; #Unnecessary for the styles task...
 
-gulp.task("default", function defaultTask(cb) {
+//Former default function - for testing purposes!
+/*gulp.task("default", function defaultTask(cb) {
   console.log("Testing, 1, 2, 3!");
   cb();
+});*/
+
+gulp.task("default", function() {
+  gulp.watch('./sass/**/*.scss', gulp.series('styles'));
 });
 
 gulp.task("styles", async function() {
